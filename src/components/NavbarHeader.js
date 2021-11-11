7faff15fb4a38370d4ff9d58d0514a5001519abe
Breno Nogueira <breno.nog.araujo@hotmail.com>
2021-11-11 00:23:24 -0400

    pagina contato

--- src/components/NavbarHeader.js
+++ src/components/NavbarHeader.js
@@ -4,58 +4,92 @@ import logo from "../assets/images/logo/logo.svg";
 import "../assets/css/animate.css";
 
 export const NavbarHeader = () => {
-    return (
-        <>
-            <header className="header-area">
-                <div className=" navbar-area sticky" >
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <nav className="navbar navbar-expand-lg">
-                                    <a className="navbar-brand" href="/"><img src={logo} alt="Logo" /></a>
-                                    
-                                    <button class="block navbar-toggler focus:outline-none lg:hidden"
-                                        type="button" data-toggle="collapse" data-target="#navbarOne" aria-controls="navbarOne"
-                                        aria-expanded="false" aria-label="Toggle navigation">
-                                        <span class=" toggler-icon relative block duration-300 bg-white h-[2px] w-[30px] my-[6px]"></span>
-                                        <span class=" toggler-icon relative block duration-300 bg-white h-[2px] w-[30px] my-[6px]"></span>
-                                        <span class=" toggler-icon relative block duration-300 bg-white h-[2px] w-[30px] my-[6px]"></span>
-                                    </button>
+  return (
+    <>
+      <header className="header-area">
+        <div className=" navbar-area sticky">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <nav className="navbar navbar-expand-lg">
+                  <a className="navbar-brand" href="/">
+                    <img src={logo} alt="Logo" />
+                  </a>
 
-                                    <div className="collapse navbar-collapse sub-menu-bar"id="navbarOne">
-                                        <ul id="nav" className="navbar-nav ml-auto">
-                                            <li className="nav-item active">
-                                                <a className="page-scroll" href="/"> Inicio </a>
-                                            </li>
+                  <button
+                    class="block navbar-toggler focus:outline-none lg:hidden"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarOne"
+                    aria-controls="navbarOne"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                  >
+                    <span class=" toggler-icon relative block duration-300 bg-white h-[2px] w-[30px] my-[6px]"></span>
+                    <span class=" toggler-icon relative block duration-300 bg-white h-[2px] w-[30px] my-[6px]"></span>
+                    <span class=" toggler-icon relative block duration-300 bg-white h-[2px] w-[30px] my-[6px]"></span>
+                  </button>
 
-                                            <li className="nav-item">
-                                                <a className="page-scroll" href="/#recursos">Recursos </a>
-                                            </li>
+                  <div
+                    className="collapse navbar-collapse sub-menu-bar"
+                    id="navbarOne"
+                  >
+                    <ul id="nav" className="navbar-nav ml-auto">
+                      <li className="nav-item active">
+                        <a className="page-scroll" href="/">
+                          Inicio
+                        </a>
+                      </li>
 
-                                            <li className="nav-item">
-                                                <a className="page-scroll" href="/#about"> Serviços </a>
-                                            </li>
+                      <li className="nav-item">
+                        <a className="page-scroll" href="/#recursos">
+                          Recursos
+                        </a>
+                      </li>
 
-                                            <li className="nav-item">
-                                                <a className="page-scroll" href="/page-abautUs"> Sobre Nós </a>
-                                            </li>
+                      <li className="nav-item">
+                        <a className="page-scroll" href="/#about">
+                          Serviços
+                        </a>
+                      </li>
 
-                                            <li className="nav-item">
-                                                <a className="page-scroll" href="page-customers"> Clientes </a>
-                                            </li>
-                                        </ul>
-                                        <div className=" navbar-btn">
-                                            <a className="main-btn gradient-btn" data-scroll-nav="1,5s" href="report"rel="nofollow">RELATÓRIO</a>
-                                        </div>
-                                    </div>
-                                </nav>
-                            </div>
-                        </div>
+                      <li className="nav-item">
+                        <a className="page-scroll" href="/page-abautUs">
+                          Sobre Nós
+                        </a>
+                      </li>
+
+                      <li className="nav-item">
+                        <a className="page-scroll" href="page-customers">
+                          Clientes
+                        </a>
+                      </li>
+
+                      <li className="nav-item">
+                        <a className="page-scroll" href="page-contato">
+                          Contato
+                        </a>
+                      </li>
+                    </ul>
+                    <div className=" navbar-btn">
+                      <a
+                        className="main-btn gradient-btn"
+                        data-scroll-nav="1,5s"
+                        href="report"
+                        rel="nofollow"
+                      >
+                        RELATÓRIO
+                      </a>
                     </div>
-                </div>
-            </header>
-        </>
-    )
-}
+                  </div>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
 
 export default NavbarHeader;
--- src/pages/PageContato/index.js
+++ src/pages/PageContato/index.js
@@ -1,8 +1,78 @@
-import React from 'react'
+import {
+  Checkbox,
+  Container,
+  FormControlLabel,
+  Grid,
+  TextField,
+  Typography,
+  Button
+} from "@material-ui/core";
+import Paper from "@mui/material/Paper";
+
+import React from "react";
 
 export default function index() {
-    return (
-      
+  return (
+    <Container maxWidth="sm">
+      <Paper
+        variant="outlined"
+        sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+      >
+        <Typography variant="h6" gutterBottom>
+          Fale Conosco
+        </Typography>
+        <form>
+        <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="sobrenome"
+              name="nome"
+              label="Nome"
+              fullWidth
+              autoComplete="given-name"
+              variant="standard"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="sobrenome"
+              name="sobrenome"
+              label="Sobrenome"
+              fullWidth
+              autoComplete="family-name"
+              variant="standard"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="email"
+              name="email"
+              label="E-mail"
+              fullWidth
+              autoComplete="email"
+              variant="standard"
+            />
+          </Grid>
 
-    )
+          <Grid item xs={12}>
+            <TextField
+              id="outlined-multiline-static"
+              label="Mensagem"
+              multiline
+              fullWidth
+              rows={4}
+            />
+          </Grid>
+        </Grid>
+        <Button variant="contained" sx={{ mt: 3, ml: 1 }}>
+          Enviar
+        </Button>
+        </form>
+       
+      </Paper>
+    </Container>
+  );
 }
